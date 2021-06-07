9c0912c51853b885f87983548a8edf9659ccebe3
NikolaiKorobkov <85480654+NikolaiKorobkov@users.noreply.github.com>
2021-06-07 17:08:09 +0300

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,52 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+
+    public class checkSumSign {
+        public static void main(String[] args) {
+            int a = -100;
+            int b = 0;
+            System.out.println("Hello");
+            if (a + b >= 0) {
+                System.out.println("Положительная");
+            } else if (a +b < 0) {
+                System.out.println("Отрицательная");
+            } else System.out.println("Unknow!");
+        }
+    }
+
+    public class PrintColor {
+        public static void main(String[] args) {
+            int a = 50;
+            if (a == 100) {
+                System.out.println("Красный");
+            } else if (a <= 100) {
+                System.out.println("Желтый");
+            } else if (a >= 100); {
+                System.out.println("Зеленый");
+            }
+        }
+    }
+        public class compareNumbers {
+            public static void main(String[] args) {
+                int a = 50;
+                int b = 0;
+                System.out.println("Hello");
+                if (a>=b) {
+                    System.out.println("a>=b");
+                } else if (a<b) {
+                    System.out.println("a<b");
+                }
+            }
+        }
+
+
+}
+
